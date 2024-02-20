790576bc930f349c4e51636c7ef66a7380f13d79
kanatas-revenge <kanatas.revenge@gmail.com>
2024-02-20 14:23:52 +0100

    Добавлен Python файл с парой строк кода

---
+++ testPython.py
@@ -0,0 +1,19 @@
+from graphics import *
+
+
+def main():
+    paint_house(200, 200, 300, 400)
+
+def paint_house(x, y, width, height):
+    """
+    Рисует домик.
+    :param x, y: - опорная точка, левая нижняя точка крыши
+    :param width: - ширина
+    :param height: - высота
+    :return: - возвращает риусуночек домика
+    """
+    pass
+
+main()
+print("Ты меня видишь?")
+print("Хочу закоммитить")
